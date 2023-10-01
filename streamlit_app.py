b48298cbaf16214b1d963241c28e7e93c80501bf
Danish Faisal <danishfaisal.wwe@gmail.com>
2023-10-01 17:50:21 +0530

    Simple chat GUI with streaming

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
+import random
+import time
 
-st.title("Echo Bot")
+st.title("Simple chat")
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -18,9 +20,23 @@ if prompt := st.chat_input("What is up?"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-    response = f"Echo: {prompt}"
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        st.markdown(response)
+        message_placeholder = st.empty()
+        full_response = ""
+        assistant_response = random.choice(
+            [
+                "Hello there! How can I assist you today?",
+                "Hi, human! Is there anything I can help you with?",
+                "Do you need help?",
+            ]
+        )
+        # Simulate stream of response with milliseconds delay
+        for chunk in assistant_response.split():
+            full_response += chunk + " "
+            time.sleep(0.05)
+            # Add a blinking cursor to simulate typing
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
     # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
